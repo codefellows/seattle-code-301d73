e4a908abcc9a5ff2e67297f911f57f0e81708cfb
JB Tellez <jb.tellez@gmail.com>
2021-04-14 06:10:34 -0700

    updates class 3 demo styling

--- class-03/demo/passing-functions/src/app.js
+++ class-03/demo/passing-functions/src/app.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Parent from './parent.js';
 import Header from './header';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends React.Component {
   render(){
     return (
+      <Container>
+
       <div className="App">
         <Header title={'Billy is a teenager'} />
         <Parent />
       </div>
+      </Container>
     );
   }
 }
