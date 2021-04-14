a31dedf13895282ae17fd6d85cae99b72b0c28be
JB Tellez <jb.tellez@gmail.com>
2021-04-14 07:10:37 -0700

    adds modal to demo

--- class-03/demo/passing-functions/src/app.js
+++ class-03/demo/passing-functions/src/app.js
@@ -4,16 +4,37 @@ import Header from './header';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import ModalExample from './modal-example'
+import Button from 'react-bootstrap/Button'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    }
+  }
+
+  handleShow = () => {
+    this.setState({showModal: true})
+  }
+
+  handleClose = () => {
+    this.setState({showModal: false});
+  }
+
   render(){
     return (
       <Container>
+          <Button variant="primary" onClick={this.handleShow}>
+            Launch demo modal
+          </Button>
 
-      <div className="App">
+        <ModalExample show={this.state.showModal} onClose={this.handleClose} />
         <Header title={'Billy is a teenager'} />
         <Parent />
-      </div>
+
       </Container>
     );
   }
