e8d297ed3ef485c5d06e0730997a0dd96b5fddfa
JB-Tellez <jb.tellez@gmail.com>
2021-04-19 14:01:14 -0700

    adds in class demo tweaks

--- class-06/demo/location/src/app.js
+++ class-06/demo/location/src/app.js
@@ -1,29 +1,49 @@
-import React from 'react';
 import axios from 'axios';
+import React from 'react';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      searchQuery:'',
+    this.state = {
+      searchQuery: '',
       location: {}
     }
   }
 
   getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-    const res = await axios.get(API);
-    console.log(res.data[0])
-    this.setState({ location:res.data[0] });
+
+    const apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+
+    const response = await axios.get(apiUrl);
+
+    const location = response.data[0];
+
+    this.setState({
+      location,
+      // same as location:location
+    });
+
+
+
+
+
   }
 
   render() {
-    return(
+
+    const img_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
+
+    console.log(img_url);
+
+    return (
       <>
         <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
         <button onClick={this.getLocation}>Explore!</button>
-        {this.state.location.place_id && 
-          <h2>The city is: {this.state.location.display_name}</h2>
+        {this.state.location.place_id &&
+          <>
+            <h2>The city is: {this.state.location.display_name}</h2>
+            <img src={img_url} alt="location" />
+          </>
         }
       </>
     )
